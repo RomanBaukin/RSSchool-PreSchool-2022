451bb73b43f61a52a81893344483c6f79a9d199c
Roman Baukin <rbaukin1984@gmail.com>
2022-01-29 17:06:25 +0400

    feat: add functionality for Portfolio section

--- portfolio/index.js
+++ portfolio/index.js
@@ -18,4 +18,26 @@ mobileMenu.addEventListener('click', (event) => {
   }
 })
 
-console.log('блок <header> +6\n секция hero +6\n секция skills +6\n секция portfolio +6\n секция video +6\n секция price +6\n секция contacts +6\n блок < footer > +6\n нет полосы прокрутки при ширине страницы от 1440 рх до 768 рх +5\n нет полосы прокрутки при ширине страницы от 768 рх до 480 рх +5\n нет полосы прокрутки при ширине страницы от 480 рх до 320 рх +5\n при ширине страницы 768 рх панель навигации скрывается, появляется бургер - иконка +2\n при нажатии на бургер - иконку справа плавно появляется адаптивное меню, бургер - иконка изменяется на крестик +4\n высота адаптивного меню занимает всю высоту экрана.При ширине экрана 768 - 620 рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана +4\n при нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер - иконку +4\n бургер - иконка, которая при клике превращается в крестик, создана при помощи css - анимаций без использования изображений+ 2\n ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +2\n при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, крестик превращается в бургер - иконку +4\n Score 85/85')
+// Tabs
+
+const tabs = document.querySelector('.tabs'),
+  tabsButtons = document.querySelectorAll('.tabs__button'),
+  gridItems = document.querySelectorAll('.grid__item');
+
+tabs.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if (target.matches('.tabs__button') && !target.matches('.tabs__button--active')) {
+    target.classList.toggle('tabs__button--active');
+
+    for (let item of tabsButtons) {
+      if (target !== item) {
+        item.classList.remove('tabs__button--active');
+      }
+    }
+
+    for (let key = 0; key < 6; key++) {
+      gridItems[key].style.backgroundImage = `url('./assets/img/${target.textContent.toLowerCase()}/0${+key+1}.jpg')`;
+    }
+  }
+});
