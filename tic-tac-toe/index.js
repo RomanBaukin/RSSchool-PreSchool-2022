575ef1582725c442ace3d740468b029477a20fc9
Roman Baukin <rbaukin1984@gmail.com>
2022-02-18 18:52:29 +0400

    feat: add button New Game & output result game

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,20 +1,12 @@
+const resultArea = document.getElementById('result');
+const moveCounter = document.getElementById('move-counter');
 const area = document.getElementById('area');
+const button = document.getElementById('button');
+const boxes = document.getElementsByClassName('box');
 let move = 0;
 let result = '';
 
-area.addEventListener('click', (event) => {
-  const target = event.target;
-
-  if (target.className === 'box') {
-    move % 2 === 0 ? target.innerHTML = '&#10008;' : target.innerHTML = '&#216;';
-    move++;
-    check();
-  }
-
-})
-
 const check = () => {
-  const boxes = document.getElementsByClassName('box');
   const winArr = [
     [0, 1, 2],
     [3, 4, 5],
@@ -28,15 +20,39 @@ const check = () => {
 
   for (const key in winArr) {
     if (boxes[winArr[key][0]].innerHTML === '✘' && boxes[winArr[key][1]].innerHTML === '✘' && boxes[winArr[key][2]].innerHTML === '✘') {
-      result = 'крестики';
+      result = 'Crosses';
       outputResult(result);
+      move = 9;
     } else if (boxes[winArr[key][0]].innerHTML === 'Ø' && boxes[winArr[key][1]].innerHTML === 'Ø' && boxes[winArr[key][2]].innerHTML === 'Ø') {
-      result = 'нолики';
+      result = 'Zeros';
       outputResult(result);
+      move = 9;
     }
   }
 }
 
 const outputResult = (winner) => {
-  console.log(`Победили ${winner}`);
+  resultArea.innerHTML = `${winner} win`;
+  moveCounter.innerHTML = `Total moves: ${move}`;
 }
+
+const newGame = () => {
+  resultArea.innerHTML = '';
+  moveCounter.innerHTML = '';
+  for (const item of boxes) {
+    item.innerHTML = '';
+  }
+  move = 0;
+}
+
+area.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if (target.className === 'box' && move < 9) {
+    move % 2 === 0 ? target.innerHTML = '&#10008;' : target.innerHTML = '&#216;';
+    move++;
+    check();
+  }
+})
+
+button.addEventListener('click', newGame);
